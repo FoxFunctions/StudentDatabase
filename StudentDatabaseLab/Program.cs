12f918d60c448b9b5742023f23f3d55c99a5e166
Austin Howell <austinhowell@Austins-MacBook-Pro.local>
2022-04-18 19:40:59 -0400

    cleaning up the code

--- StudentDatabaseLab/Program.cs
+++ StudentDatabaseLab/Program.cs
@@ -72,7 +72,6 @@ namespace StudentDatabaseLab
 
                 while (true)
                 {
-
                     Console.WriteLine($"You have chosen {studentName[navChoice]}. What would you like to know? Please enter hometown or favorite food. ");
                     string navigationChoice = Console.ReadLine().ToLower().Trim();
 
@@ -88,7 +87,6 @@ namespace StudentDatabaseLab
                         Console.WriteLine();
                         break;
                     }
-                    
                     else
                     {
                         Console.WriteLine("Sorry, I didn't quite get that. Please choose hometown or favorite food.");
@@ -117,32 +115,3 @@ namespace StudentDatabaseLab
         }
     }
 }
-
-
-
-/* STUDENT DATABASE
-Objectives: Storing Data in Arrays, Searching Data
-
-Task: Write a program that will recognize invalid inputs when the user requests information about students in a class.
-
-What will the application do?
-2 Points: Create 3 arrays and fill them with student information—one with name, one with hometown, one with favorite food
-1 Point: Prompt the user to ask about a particular student by number. Convert the input to an integer. Use the integer as the index for the arrays. Print the student’s name.
-1 Point: Ask the user which category to display: Hometown or Favorite food. Then display the relevant information.
-1 Point: Ask the user if they would like to learn about another student.
-
-Build Specifications:
-1 Point: Validate user number: Use an if statement to check if the number is out of range, i.e. either less than 0 or greater than the maximum index of the arrays. If so, display a friendly message and let the user try again.
-1 Point: Validate category: Ask the user what information category to display: "Hometown" or "Favorite Food". Use an if statement to check that they entered either category name correctly. If they entered an incorrect category, display a friendly message and re-ask the question.
-1 Point: Array Length: Use the first array’s Length property in your code instead of hardcoding it.
-
-Hints:
-Make sure the arrays are the same size.
-Let the user enter a number from 1 up to and including the length of the array. To get the index, subtract 1 from the number they entered.
-Make it easy for the user – tell them what information is available
-Try to use good grammar. Make your messages polite.
-
-Extra Challenges:
-1 Point: Provide an option where the user can see a list of all students.
-2 Points: Allow the user to search by student name
-1 Point: Category names: Allow uppercase and lowercase; allow portion of word such as "Food" instead of "Favorite Food" */
